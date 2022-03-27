40dbeaef164c86dd1ba512d16d0324e0bf3c39b3
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2022-03-27 14:05:33 +0530

    Add GPL-3.0 license

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 "use strict";
-exports.__esModule = true;
-var fs = require("fs");
-var child_process_1 = require("child_process");
-process.chdir("C:\\coding\\Node.js\\MindustryLauncher");
-var settings = parseJSONC(fs.readFileSync("config.json", "utf-8"));
-var mindustryProcess;
-var currentLogStream;
+/******************************
+ *
+ * This program is free software:
+ * you can redistribute it and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs = require("fs");
+const child_process_1 = require("child_process");
+process.chdir(`C:\\coding\\Node.js\\MindustryLauncher`);
+const settings = parseJSONC(fs.readFileSync("config.json", "utf-8"));
+let mindustryProcess;
+let currentLogStream;
 function parseArgs(args) {
-    var parsedArgs = {};
-    var argName = "null";
-    for (var _i = 0, args_1 = args; _i < args_1.length; _i++) {
-        var arg = args_1[_i];
+    let parsedArgs = {};
+    let argName = "null";
+    for (let arg of args) {
         if (arg.startsWith("--")) {
             argName = arg.slice(2);
             parsedArgs[arg.toLowerCase().slice(2)] = "null";
@@ -23,9 +35,9 @@ function parseArgs(args) {
     return parsedArgs;
 }
 function startProcess(_filePath, _jvmArgs) {
-    var proc = (0, child_process_1.spawn)("java", ["-jar " + _filePath].concat(_jvmArgs).join(" ").split(" "));
-    var d = new Date();
-    currentLogStream = fs.createWriteStream("" + settings.logging.path + d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate() + "--" + d.getHours() + "-" + d.getMinutes() + "-" + d.getSeconds() + ".txt");
+    const proc = (0, child_process_1.spawn)("java", [`-jar ${_filePath}`].concat(_jvmArgs).join(" ").split(" "));
+    const d = new Date();
+    currentLogStream = fs.createWriteStream(`${settings.logging.path}${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}--${d.getHours()}-${d.getMinutes()}-${d.getSeconds()}.txt`);
     proc.stdout.pipe(process.stdout);
     proc.stdout.pipe(currentLogStream);
     proc.stderr.pipe(process.stderr);
@@ -39,36 +51,35 @@ function restart(_filePath, _jvmArgs) {
     console.log("Started new process.");
 }
 function copyMods() {
-    for (var _i = 0, _a = settings.externalMods; _i < _a.length; _i++) {
-        var file = _a[_i];
-        console.log("Copying mod " + file);
-        var modname = file.match(/(?<=[/\\])[^/\\:*?"<>]+?(?=(Desktop)?\.jar$)/i); //hello regex my old friend
+    for (var file of settings.externalMods) {
+        console.log(`Copying mod ${file}`);
+        let modname = file.match(/(?<=[/\\])[^/\\:*?"<>]+?(?=(Desktop)?\.jar$)/i); //hello regex my old friend
         if (modname == null) {
-            throw new Error("Invalid mod filename " + file + "!");
+            throw new Error(`Invalid mod filename ${file}!`);
         }
-        fs.copyFileSync(file, process.env["appdata"] + "\\Mindustry\\mods\\" + modname[0] + ".jar");
+        fs.copyFileSync(file, `${process.env["appdata"]}\\Mindustry\\mods\\${modname[0]}.jar`);
     }
 }
 function main() {
-    var _a;
-    var state = "normal";
-    for (var _i = 0, _b = Object.entries(settings.mindustryJars.versionNames); _i < _b.length; _i++) {
-        var _c = _b[_i], version = _c[0], jarName_1 = _c[1];
-        if (jarName_1.includes(" ")) {
-            throw new Error("Jar name for version " + version + " contains a space.");
+    let state = "normal";
+    for (let [version, jarName] of Object.entries(settings.mindustryJars.versionNames)) {
+        if (jarName.includes(" ")) {
+            throw new Error(`Jar name for version ${version} contains a space.`);
         }
     }
-    var parsedArgs = parseArgs(process.argv.slice(2));
+    let parsedArgs = parseArgs(process.argv.slice(2));
     if (parsedArgs["help"]) {
-        console.log("Usage: mindustry [--help] [--version <version>]\n\t--help\tDisplays this help message and exits.\n\t--version\tSpecifies the version to use.");
+        console.log(`Usage: mindustry [--help] [--version <version>]
+	--help\tDisplays this help message and exits.
+	--version\tSpecifies the version to use.`);
         process.exit();
     }
-    var jarName = (_a = settings.mindustryJars.versionNames[parsedArgs["version"]]) !== null && _a !== void 0 ? _a : settings.mindustryJars.versionNames["135"];
-    var filePath = jarName.match(/[/\\]/gi) ? jarName : settings.mindustryJars.folderPath + jarName;
-    console.log("Launching Mindustry version " + (settings.mindustryJars.versionNames[parsedArgs["version"]] ? parsedArgs["version"] : "135"));
+    let jarName = settings.mindustryJars.versionNames[parsedArgs["version"]] ?? settings.mindustryJars.versionNames["135"];
+    let filePath = jarName.match(/[/\\]/gi) ? jarName : settings.mindustryJars.folderPath + jarName;
+    console.log(`Launching Mindustry version ${settings.mindustryJars.versionNames[parsedArgs["version"]] ? parsedArgs["version"] : "135"}`);
     copyMods();
     mindustryProcess = startProcess(filePath, settings.jvmArgs);
-    process.stdin.on("data", function (data) {
+    process.stdin.on("data", (data) => {
         switch (data.toString("utf-8").slice(0, -2)) {
             case "rs":
             case "restart":
@@ -79,19 +90,18 @@ function main() {
                 break;
         }
     });
-    mindustryProcess.on("exit", function (statusCode) {
+    mindustryProcess.on("exit", (statusCode) => {
         if (statusCode == 0) {
             console.log("Process exited.");
         }
         else {
-            console.log("Process crashed with exit code " + statusCode + "!");
+            console.log(`Process crashed with exit code ${statusCode}!`);
         }
         process.exit();
     });
-    for (var _d = 0, _e = settings.externalMods; _d < _e.length; _d++) {
-        var file = _e[_d];
-        fs.watchFile(file, function () {
-            console.log("File change detected! (" + file + ")");
+    for (var file of settings.externalMods) {
+        fs.watchFile(file, () => {
+            console.log(`File change detected! (${file})`);
             copyMods();
             if (settings.restartAutomaticallyOnModUpdate)
                 restart(filePath, settings.jvmArgs);
@@ -101,7 +111,7 @@ function main() {
 main();
 function parseJSONC(data) {
     return JSON.parse(data.split("\n")
-        .filter(function (line) { return !/^[ \t]*\/\//.test(line); })
+        .filter(line => !/^[ \t]*\/\//.test(line))
         .join("\n"));
-    //Removes lines that start with any amount of whitespaces and two forward slashes(comments).
+    //Removes lines that start with any amount of whitespaces or tabs and two forward slashes(comments).
 }
